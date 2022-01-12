d47a939cb91f41f6f723372baa06fa90fd281623
mimwin <kms105903@naver.com>
2022-01-12 23:29:25 +0900

    Change to Storeis

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import {Text} from 'react-native'
-import ProfilePicture from '../../components/ProfilePicture';
+import Stories from '../../components/Stories';
 
 
 const HomeScreen =() => {
 
     <>
         <Text>Hello World</Text>
-        <ProfilePicture uri={'file:///C:/Users/kms10/Desktop/images.jpeg'}/>
-        <ProfilePicture uri={'file:///C:/Users/kms10/Desktop/images.jpeg'}/>
-        <ProfilePicture uri={'file:///C:/Users/kms10/Desktop/images.jpeg'}/>
+        <Stories />
     </>
 
 }
